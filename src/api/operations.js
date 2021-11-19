7f503314c774be7c3bb5f5080755b2646029f934
MonikaJedrzejak <jedrzejakmonika@tlen.pl>
2021-11-19 22:31:15 +0100

    first list of movies

--- src/api/operations.js
+++ src/api/operations.js
@@ -3,16 +3,16 @@ const API_KEY = "53eedff4";
 
 export function getMovies(search, cb) {
     const API_URL = `https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`;
-    fetch(`${API_URL}${search}`)
+    fetch(`${API_URL}`)
     .then((res) => {
       if (res.ok) {
         return res.json();
       }
       throw new Error("Ups...");
     })
-    .then(( results ) => {
-      console.log(results);
-      cb(results);
+    .then(( {Search, totalResults}) => {
+      console.log(totalResults);
+      cb(Search);
     })
     .catch((err) => console.log(err));
 }
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -4,36 +4,44 @@ import {getMovies} from '../api/operations';
 
 export default function SearchResults() {
     const [movie, setMovie] = useState({});
-    const [searchQuery, setSearchQuery] = useState('');
-
-  //   useEffect(() => {
-  //     getMovies(movie, setMovie);
-  // }, [search]);
-
-
-    const API_KEY = "53eedff4";
-    const url = `https://www.omdbapi.com/?apikey=${API_KEY}&s=${searchQuery}`;
+    const [searchQuery, setSearchQuery] = useState('pooh');
 
     useEffect(() => {
-      fetch(url)
-        .then((res) => {
-          if (res.ok) {
-            return res.json();
-          }
-          throw new Error("Ups...");
-        })
-        .then(( {Search, totalResults} ) => {
-          console.log(Search);
-          console.log(totalResults);
-          setMovie(Search);
-        })
-        .catch((err) => console.log(err));
-    }, [searchQuery]);
+      getMovies(searchQuery, setMovie);
+      console.log(movie);
+  }, [searchQuery]);
 
+
+    // const API_KEY = "53eedff4";
+    // const url = `https://www.omdbapi.com/?apikey=${API_KEY}&s=${searchQuery}`;
+
+    // useEffect(() => {
+    //   fetch(url)
+    //     .then((res) => {
+    //       if (res.ok) {
+    //         return res.json();
+    //       }
+    //       throw new Error("Ups...");
+    //     })
+    //     .then(( {Search, totalResults} ) => {
+    //       console.log(Search);
+    //       console.log(totalResults);
+    //       setMovie(Search);
+    //     })
+    //     .catch((err) => console.log(err));
+    // }, [searchQuery]);
+
+    let pageList = [];
+    pageList = movie;
+    console.log(pageList);
     return (
         <div>
           <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery}/>
             SearchResults
+            <ul>
+              {pageList.length ? (pageList.map((el) => <li key={el.imdbID}>{el.Title}</li>)) : (<p>loading...</p>)}
+            </ul>
         </div>
     )
 }
+//TO DO ogarnij blad -  brak wynikow
